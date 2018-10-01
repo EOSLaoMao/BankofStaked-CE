6b6787e6dfdde07722d2a21a7577b6e6bd79ae18
ZhaoYu <zhaoyu.johnny@gmail.com>
2018-10-01 18:36:19 +0800

    add new creditor accounts

--- scripts/add_creditor.sh
+++ scripts/add_creditor.sh
@@ -11,6 +11,9 @@ cleos -u $API push action bankofstaked addcreditor '{"account": "acroeosrndev", 
 cleos -u $API push action bankofstaked addcreditor '{"account": "bankofeosys2", "for_free": 1, "free_memo": "A gift from EOSYS"}' -p bankofstaked
 cleos -u $API push action bankofstaked addcreditor '{"account": "bankofeosys1", "for_free": 0, "free_memo": ""}' -p bankofstaked
 cleos -u $API push action bankofstaked addcreditor '{"account": "eosasia.bp", "for_free": 1, "free_memo": "A gift from block producer eosasia11111"}' -p bankofstaked
+cleos -u $API push action bankofstaked addcreditor '{"account": "eoseco.bp", "for_free": 1, "free_memo": "A gift from EOSeco"}' -p bankofstaked
+cleos -u $API push action bankofstaked addcreditor '{"account": "eospacestake", "for_free": 1, "free_memo": "A gift from EOSpace"}' -p bankofstaked
+cleos -u $API push action bankofstaked addcreditor '{"account": "bankofstkarg", "for_free": 1, "free_memo": "A gift from EOS Argentina"}' -p bankofstaked
 #cleos -u $API push action bankofstaked activate '{"account": "charity.bank"}' -p bankofstaked
 #cleos -u $API push action bankofstaked activate '{"account": "staking.bank"}' -p bankofstaked
 #cleos -u $API push action bankofstaked activate '{"account": "eosbeijinghp"}' -p bankofstaked
