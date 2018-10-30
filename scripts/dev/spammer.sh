a4014d588692c0d1046e782c7d7f6666d105f4ce
ZhaoYu <zhaoyu.johnny@gmail.com>
2018-10-30 18:33:56 +0900

    using wellknown key in spammer.sh

--- scripts/dev/spammer.sh
+++ scripts/dev/spammer.sh
@@ -5,7 +5,7 @@ do
   for j in a b c d e f g h i j k l m n o p q r s t u v w x y z
   do
     name=testaccoua$i$j
-    cleos system newaccount voter3 $name EOS5M6M6WPV2ncSquidJTGmV8xeoFRdjAK9M2veZusYoneQCLYziq EOS5M6M6WPV2ncSquidJTGmV8xeoFRdjAK9M2veZusYoneQCLYziq --stake-net "10 EOS" --stake-cpu "10 EOS" --buy-ram "10 EOS"
+    cleos system newaccount voter3 $name EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV --stake-net "10 EOS" --stake-cpu "10 EOS" --buy-ram "10 EOS"
     cleos transfer voter3 bankofstaked '0.1 EOS' "$name"
   done
 done
